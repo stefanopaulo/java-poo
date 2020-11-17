4689c5536be63b00d6bc77448e1d55eeac6d2a8f
stefanopaulo <stefanopaulo47@gmail.com>
2020-11-17 18:32:51 -0300

    agregação entre objetos com java

--- uec/src/Fight.java
+++ uec/src/Fight.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Fight {
 	private Fighter challenged;
@@ -5,12 +6,53 @@ public class Fight {
 	private int rounds;
 	private boolean okay;
 	
-	public void markFight() {
-		
+	public void markFight(Fighter f1, Fighter f2) {
+		if (f1.getCategory().equals(f2.getCategory()) && f1 != f2) {
+			this.setOkay(true);
+			this.setChallenged(f1);
+			this.setChallenger(f2);
+		} else {
+			this.setOkay(false);
+			this.setChallenged(null);
+			this.setChallenger(null);
+		}
 	}
 	
 	public void fight() {
-		
+		if (this.isOkay()) {
+			this.getChallenged().toPresent();
+			this.getChallenged().status();
+			this.getChallenger().toPresent();
+			this.getChallenger().status();
+			
+			Random random = new Random();
+			
+			int winner = random.nextInt(3);
+			
+			switch (winner) {
+				case 0:
+					System.out.println("Empatou!");
+					System.out.println();
+					this.getChallenged().tieFight();
+					this.getChallenger().tieFight();
+					break;
+				case 1:
+					System.out.println(this.getChallenged().getName() + " ganhou a luta!");
+					System.out.println();
+					this.getChallenged().winFight();
+					this.getChallenger().loseFight();
+					break;
+				case 3:
+					System.out.println(this.getChallenger().getName() + " ganhou a luta!");
+					System.out.println();
+					this.getChallenged().loseFight();
+					this.getChallenger().winFight();
+					break;
+			}
+			
+		} else {
+			System.out.println("Luta não pode acontecer!");
+		}
 	}
 
 	public Fighter getChallenged() {
